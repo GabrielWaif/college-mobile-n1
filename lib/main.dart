0b4f4d8fabcb46e16dbb2ee89b9ee3ae55935ba0
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2023-09-26 20:53:14 -0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const greyColor = Color.fromARGB(255, 97, 97, 96);
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            background: const Color.fromARGB(249, 249, 250, 255)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,48 +33,114 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Column(
+      body: Column(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 70, left: 40, right: 40),
+          Container(
+            padding: const EdgeInsets.all(30),
+            margin: const EdgeInsets.only(top: 50, left: 20, right: 20),
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(254, 254, 255, 255),
+                borderRadius: BorderRadius.all(Radius.circular((20)))),
             child: Column(
               children: [
-                Row(children: [
+                const Row(children: [
                   Text(
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 22),
                     'Trending',
                   ),
-                  Icon(Icons.chevron_right),
+                  Icon(
+                    Icons.chevron_right,
+                  )
                 ]),
-                Divider(),
-                // A Post Container here
+                const Divider(
+                  color: Color.fromARGB(245, 244, 245, 255),
+                ),
                 Column(
                   children: [
-                    CircleAvatar(),
-                    Row(
-                      children: [Text("r/AskReddit")],
-                    ),
-                    Text(
-                        "dlsfjlsdjfksj jsldfj skldjfl kjsdlkf jsldkfjlsdjlfkjsdjkflksdjflsdjlfjsljfsdfljsldfjlsjdf lsjlfjs"),
-                    Text("by r/cochieforbreakfast * 7h"),
-                    Row(
+                    const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Row(
-                          children: [Icon(Icons.arrow_upward), Text("28,2K")],
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 20),
+                              child: CircleAvatar(
+                                foregroundImage: NetworkImage(
+                                    "https://freelogopng.com/images/all_img/1658834095reddit-logo-png.png"),
+                              ),
+                            ),
+                            Text("r/AskReddit")
+                          ],
                         ),
-                        Icon(Icons.arrow_downward),
-                        Icon(Icons.chat_bubble_outline),
-                        Icon(Icons.share_outlined)
+                        Icon(Icons.more_vert),
+                      ],
+                    ),
+                    const Text(
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.w500),
+                        "dlsfjlsdjfksj jsldfj skldjfl kjsdlkf jsldkfjlsdjlfkjsdjkflksdjflsdjlfjsljfsdfljsldfjlsjdf lsjlfjs"),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Image.network(
+                            "https://miro.medium.com/v2/resize:fit:640/format:webp/0*3wWQsMvkaFTc0JMG.png"),
+                      ),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.only(bottom: 20, top: 10),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 152, 153, 152),
+                                fontSize: 15),
+                            "by r/cochieforbreakfast * 7h",
+                          ),
+                        )),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child:
+                                    Icon(color: greyColor, Icons.arrow_upward)),
+                            Text("28,2K",
+                                style:
+                                    TextStyle(color: greyColor, fontSize: 15)),
+                          ],
+                        ),
+                        Icon(color: greyColor, Icons.arrow_downward),
+                        Row(
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: Icon(
+                                    color: greyColor,
+                                    Icons.chat_bubble_outline)),
+                            Text(
+                                style:
+                                    TextStyle(color: greyColor, fontSize: 15),
+                                "3,0K"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: Icon(
+                                    color: greyColor, Icons.share_outlined)),
+                            Text(
+                                style:
+                                    TextStyle(color: greyColor, fontSize: 15),
+                                "Share"),
+                          ],
+                        ),
                       ],
                     )
                   ],
@@ -123,43 +193,6 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-    );
-  }
-}
-
-class HomePost extends StatelessWidget {
-  const HomePost({
-    super.key,
-    required String username,
-    required String text,
-    required String subreddit,
-    required String hoursAgo,
-    required String likes,
-    required String comments,
-  }) ;
-
-  @override
-  Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CircleAvatar(),
-        Row(
-          children: [Text("subreddit")],
-        ),
-        Text(
-            "dlsfjlsdjfksj jsldfj skldjfl kjsdlkf jsldkfjlsdjlfkjsdjkflksdjflsdjlfjsljfsdfljsldfjlsjdf lsjlfjs"),
-        Text("by r/cochieforbreakfast * 7h"),
-        Row(
-          children: [
-            Row(
-              children: [Icon(Icons.arrow_upward), Text("28,2K")],
-            ),
-            Icon(Icons.arrow_downward),
-            Icon(Icons.chat_bubble_outline),
-            Icon(Icons.share_outlined)
-          ],
-        )
-      ],
     );
   }
 }
