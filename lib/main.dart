5c4a6872a59000225adc96a6f219310c2d61cedc
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2023-09-27 18:43:03 -0300

    Tela de profile 1

--- lib/main.dart
+++ lib/main.dart
@@ -97,21 +97,28 @@ class MyHomePage extends StatelessWidget {
         type: BottomNavigationBarType.fixed,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
             icon: Icon(color: orangeColor, Icons.home_outlined),
             label: '',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(color: lightGreyColor, Icons.language),
             label: '',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(color: lightGreyColor, Icons.maps_ugc_outlined),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(color: lightGreyColor, Icons.inbox),
+            icon: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ProfilePage()));
+                },
+                icon: const Icon(color: lightGreyColor, Icons.inbox)),
             label: '',
           )
         ],
@@ -505,5 +512,269 @@ class PostPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: orangeColor,
+        body: const Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 40),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: CircleAvatar(
+                          foregroundImage: NetworkImage(
+                              "https://static9.depositphotos.com/1550494/1195/i/450/depositphotos_11950254-Urban-asian-man-with-red-sunglasses-and-skateboard-sitting-on-stairs.-Good-looking.-Cool-guy.-Wearing-grey-shirt-and-jeans.-Old-neglected-building-in-the-background..jpg"),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20),
+                                "u/figmaUser21"),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(color: Colors.white),
+                                "* online"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 50),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Icon(color: Colors.white, Icons.ac_unit),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Text(
+                                    style: TextStyle(color: Colors.white), "1"),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Text(
+                                    style: TextStyle(color: Colors.white),
+                                    "Karma"),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 10),
+                          child: Icon(color: Colors.white, Icons.table_rows),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                  style: TextStyle(color: Colors.white), "0d"),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                  style: TextStyle(color: Colors.white),
+                                  "Reddit age"),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 70),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child:
+                                Icon(color: Colors.white, Icons.person_outline),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(color: Colors.white),
+                                "My Profile"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Icon(color: Colors.white, Icons.dataset),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(color: Colors.white),
+                                "Reddit Coins"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Icon(
+                                color: Colors.white,
+                                Icons.workspace_premium_outlined),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(color: Colors.white),
+                                "Reddit Primium"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Icon(
+                                color: Colors.white, Icons.bookmark_outline),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(color: Colors.white), "Saved"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Icon(
+                                color: Colors.white, Icons.schedule_outlined),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                                style: TextStyle(color: Colors.white),
+                                "History"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )),
+        bottomNavigationBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100),
+          child: Container(
+            padding: const EdgeInsets.all(50),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(
+                          right: 20,
+                        ),
+                        child: Icon(
+                          color: Colors.white,
+                          Icons.settings_outlined,
+                        )),
+                    Padding(
+                        padding: EdgeInsets.only(right: 40),
+                        child: Text(
+                            style: TextStyle(color: Colors.white), "Settings")),
+                    Padding(
+                        padding: EdgeInsets.only(right: 40),
+                        child: Text(
+                            style: TextStyle(color: lightGreyColor), "|")),
+                    Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: Text(
+                            style: TextStyle(color: Colors.white), "Log out")),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
